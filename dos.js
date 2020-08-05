ab460fb2ca936a23c06c246182ab30494d77a6eb
Camila Iglesias Guerrero <ciglesias@sistran.com.ar>
2020-08-05 20:17:30 -0300

    Actualización ejercicios

--- dos.js
+++ dos.js
@@ -1,3 +1,15 @@
+/*
+Se debe pedir al usuario, mientras quiera:
+	- Nombre de artículo
+	- Categoría
+	- Precio.
+Al finalizar, se debe informar:
+	- Nombre de artículo con mayor precio
+	- Nombre de artículo con mayor precio, de la categoría lácteos.
+	- Cantidad de artículos ingresados de categoría bebidas
+Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas y que el precio no puede ser menor a 0 ni mayor a 1000.
+*/
+
 function mostrar()
 {
 	alert("Hola Mundo");
